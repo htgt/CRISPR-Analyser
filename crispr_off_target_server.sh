c11dfb91ec7b1b65b41abaa36c102d257ff0b187
Peter Keen <pk8@sanger.ac.uk>
2016-09-12 12:25:50 +0100

    Off target server start up script

---
+++ crispr_off_target_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting Crispr Off-Target server"
+
+DATE=`date +%d-%m-%Y`
+./bin/ots_server -c conf/indexes_nfs.conf >& /var/tmp/ots_logs/ots_$DATE.log &
+
+echo "Crispr off-target server started with process ID: $!"
+echo "Logs stored in /var/tmp/ots_logs/ots_$DATE.log"
