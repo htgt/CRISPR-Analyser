e3e06319020f494ecfb248c5cfee07b6745aab95
Peter Keen <pk8@sanger.ac.uk>
2016-09-19 14:15:12 +0100

    Startup script change

--- bin/crispr_off_target_server.sh
+++ bin/crispr_off_target_server.sh
@@ -3,6 +3,7 @@
 echo "Starting Crispr Off-Target server"
 
 DATE=`date +%d-%m-%Y`
+cd /nfs/team87/CRISPR-Analyser/
 ./bin/ots_server -c conf/indexes_nfs.conf >& /var/tmp/ots_logs/ots_$DATE.log &
 
 echo "Crispr off-target server started with process ID: $!"
